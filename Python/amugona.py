be8ceb31801619595f3697baeaec443606376c92
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-18 16:07:48 +0900

    [Feat] 240318

--- Python/amugona.py
+++ Python/amugona.py
@@ -769,3 +769,15 @@ for i in range(M):
         print(1)
     else:
         print(0)
+        
+# 10773
+ls = []
+K = int(input())
+for i in range(1,K+1):
+    num = int(input())
+    if num == 0:
+        ls.pop()
+    else:
+        ls.append(num)
+print(sum(ls))
+    
