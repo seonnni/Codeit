898ed8c8ce0fc759e6f9548ffe05498d7b9315a6
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-20 15:00:27 +0900

    [Feat] 240320

--- Python/amugona.py
+++ Python/amugona.py
@@ -815,4 +815,12 @@ for _ in range(N):
             print(dq[-1])
         else:
             print(-1)
-    
+
+# 15688
+import sys
+N = int(sys.stdin.readline())
+ls = []
+for i in range(N):
+    ls.append(int(sys.stdin.readline()))
+for i in sorted(ls):
+    print(i)
