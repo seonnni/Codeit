12f4ba13aa93938093f30866308221352afeda6a
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-21 13:49:45 +0900

    [Feat] 240321

--- Python/amugona.py
+++ Python/amugona.py
@@ -824,3 +824,15 @@ for i in range(N):
     ls.append(int(sys.stdin.readline()))
 for i in sorted(ls):
     print(i)
+
+# 10814
+N = int(input())
+user = []
+for _ in range(N):
+    age, name = map(str, input().split())
+    user.append([int(age),name])
+
+user = sorted(user, key=lambda x: x[0])
+
+for i in user:
+    print(*i)
