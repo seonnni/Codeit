c735cd2174543c254b6a8e728c794059b25f89c1
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-22 13:05:51 +0900

    [Feat] 240322

--- Python/amugona.py
+++ Python/amugona.py
@@ -836,3 +836,12 @@ user = sorted(user, key=lambda x: x[0])
 
 for i in user:
     print(*i)
+    
+# 10867
+N = int(input())
+ls = list(map(int, input().split()))
+ls = list(set(ls)) # set은 중복을 허용하지 않음
+ls.sort()
+
+for i in ls:
+    print(i, end=' ')
