fccc311562c04339d04d027e1fc30feb89713ab7
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-23 20:41:21 +0900

    [Feat] 240323

--- Python/amugona.py
+++ Python/amugona.py
@@ -845,3 +845,16 @@ ls.sort()
 
 for i in ls:
     print(i, end=' ')
+    
+# 17206
+T = int(input())
+N = list(map(int, input().split()))
+ls = [0] * 80001
+ans = 0
+
+for i in range(80001):
+    if i % 3 == 0 or i % 7 == 0:
+        ans += i
+    ls[i] = ans
+for j in N:
+    print(ls[j])  
