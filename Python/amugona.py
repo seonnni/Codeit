ae5d70dbc7e7a653fc457ac5d0e7877c03634631
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-24 22:06:08 +0900

    [Feat] 240324

--- Python/amugona.py
+++ Python/amugona.py
@@ -858,3 +858,14 @@ for i in range(80001):
     ls[i] = ans
 for j in N:
     print(ls[j])  
+
+# 11650
+N = int(input())
+ls = []
+for i in range(N):
+    x = list(map(int, input().split()))
+    ls.append(x)
+ls.sort()
+for i in range(N):
+    print(ls[i][0],ls[i][1])
+
