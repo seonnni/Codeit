8c572dd896377db2215b01d0f92f19c51dce9c4e
SEOEUN HWANG <ssjse1103@naver.com>
2024-03-25 17:01:43 +0900

    [Feat] 240325

--- Python/amugona.py
+++ Python/amugona.py
@@ -869,3 +869,32 @@ ls.sort()
 for i in range(N):
     print(ls[i][0],ls[i][1])
 
+# 10828
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+dq = deque()
+
+for _ in range(N):
+    command = list(input().split())
+    if command[0] == 'push':
+        dq.append(command[1])
+    elif command[0] == 'pop':
+        if len(dq) > 0 :
+            tmp = dq.pop()
+            print(tmp)
+        else : print(-1)
+    elif command[0] == 'size':
+        print(len(dq))
+    elif command[0] == 'empty':
+        if len(dq) > 0 :
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'top':
+        if len(dq) > 0:
+            print(dq[-1])    
+        else: print(-1)
+
