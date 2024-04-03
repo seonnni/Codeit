292d13a1e0833847f532ae1d3320a0f5844afd7a
SEOEUN HWANG <ssjse1103@naver.com>
2024-04-03 13:32:42 +0900

    [Feat] 240403

--- Python/amugona.py
+++ Python/amugona.py
@@ -1044,3 +1044,17 @@ else :
 # 2752
 num = list(map(int, input().split()))
 print(*sorted(num))
+
+# 2798
+N,M = map(int, input().split())
+ls = list(map(int, input().split()))
+result = 0
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1,N):
+            if ls[i] + ls[j] + ls[k] > M:
+                continue
+            else:
+                result = max(result, ls[i] + ls[j] + ls[k])
+
+print(result)
