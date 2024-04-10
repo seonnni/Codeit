e43d1c2499fdbb9a9f88323347a32bb91e097b56
SEOEUN HWANG <ssjse1103@naver.com>
2024-04-10 22:18:50 +0900

    [Feat] 240410

--- Python/amugona.py
+++ Python/amugona.py
@@ -1102,4 +1102,13 @@ for i in range(n):
     a,b = map(int, input().split())
     print(math.lcm(a,b))
     
-         
+# 1676
+import math
+n = int(input())
+t = 0
+for i in str(math.factorial(n)) [::-1]:
+    if i != '0':
+        break
+    else:
+        t += 1
+print(t)
