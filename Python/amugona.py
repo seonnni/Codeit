2e1a0b3ed5022459f5be91339c619f7e571d1708
SEOEUN HWANG <ssjse1103@naver.com>
2024-04-12 13:25:54 +0900

    [Feat] 240412

--- Python/amugona.py
+++ Python/amugona.py
@@ -1119,3 +1119,20 @@ ls = sys.stdin.readline().rstrip()
 ls = sorted(ls, reverse=True)
 print(''.join(ls))
 
+# 1463
+import sys
+N = int(sys.stdin.readline())
+d = [0]*(N+1)
+for i in range(2,N+1):
+    d[i] = d[i-1] + 1
+    
+    if i % 2 == 0:
+        d[i] = min(d[i],d[i//2]+1)
+        
+    if i % 3 == 0:
+        d[i] = min(d[i],d[i//3]+1)
+
+print(d[N])
+
+    
+
