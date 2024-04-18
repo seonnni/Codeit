0832fe227919fdaa3032d3df446cb083467d9fd6
SEOEUN HWANG <ssjse1103@naver.com>
2024-04-18 22:48:25 +0900

    [Feat] 240418

--- Python/amugona.py
+++ Python/amugona.py
@@ -1238,3 +1238,16 @@ while n > 0:
     else:
         ans += min_package
 print(ans)
+
+# 4796
+i = 0
+while True:
+    i += 1
+    l, p, v = map(int, input().split())
+    if l==0 and p==0 and v==0:
+        break
+    a = v//p
+    b = v%p
+    if l<b:
+        b = l
+    print("Case %d: %d" %(i, a*l+b))
