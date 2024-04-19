f9d6fd651f82687d5ed76a8da9c186d1b6a5b2b0
SEOEUN HWANG <ssjse1103@naver.com>
2024-04-19 11:55:50 +0900

    [Feat] 240419

--- Python/amugona.py
+++ Python/amugona.py
@@ -1251,3 +1251,20 @@ while True:
     if l<b:
         b = l
     print("Case %d: %d" %(i, a*l+b))
+    
+# 1748
+import sys
+n = sys.stdin.readline().strip()
+result = 0
+
+if len(n) == 1:
+    result = int(n)
+else:
+    result = 11
+    for i in range(2, 10):
+        if len(n) == i:
+            result += i*(int(n)-10**(i-1))
+            break
+        else:
+            result += i*(9*10**(i-1))+1
+print(result)
