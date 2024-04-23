c9bce69d2be111c581f1af5508f9193099ee11f7
SEOEUN HWANG <ssjse1103@naver.com>
2024-04-24 00:04:15 +0900

    [Feat] 240423

--- Python/amugona.py
+++ Python/amugona.py
@@ -1303,3 +1303,19 @@ ls.sort(key=len)
 
 for i in ls:
     print(i)
+
+# 2108
+import sys
+import statistics as st
+N = int(sys.stdin.readline())
+ls = []
+for i in range(N):
+    ls.append(int(sys.stdin.readline()))
+    
+print(round(st.mean(ls)))
+print(st.median(ls))
+a = st.multimode(ls)
+a.sort()
+m = a[1] if len(a) > 1 else a[0]
+print(m)
+print(max(ls)-min(ls))
